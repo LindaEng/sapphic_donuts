dd2beab5cc5bc1cc8a7cc0b210d56d2813c9983e
LindaEng <90lineng@gmail.com>
2024-05-18 14:10:17 -0400

    hero rendered center of screen

--- Overworld.js
+++ Overworld.js
@@ -14,8 +14,8 @@ class Overworld {
         image.src="/images/maps/DemoLower.png"
 
         //person
-        const x = 0
-        const y = 0
+        const x = 5
+        const y = 6
         const hero = new Image()
         hero.onload = () => {
             this.ctx.drawImage(
@@ -24,8 +24,8 @@ class Overworld {
                 0, //top cut
                 32, //width of cut (size px)
                 32, //height of cut (size px)
-                x, //placement x axis
-                y, //placement y axis
+                x * 16 - 8, //placement x axis
+                y * 16 - 16, //placement y axis
                 32, //size of character
                 32 // size of character
             )
